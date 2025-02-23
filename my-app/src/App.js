8e78a043c31da8796881960a42b09df585801e77
wyattcasss <58782405+wyattcasss@users.noreply.github.com>
2025-02-22 21:09:31 -0500

    Beautified and continued to work
    added a little summarry and add some design elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCard from './ProjectCard.js';
+import './App.css';
 
 const projects = [
   {
@@ -26,17 +27,28 @@ const projects = [
     liveLink: "https://example.com/live",
     githubLink: "https://github.com/wyattcasss/ValorantML"
   },
-  
+  {
+    title: "Valorant ML",
+    description: "A project built using React, Node.js, and MongoDB.",
+    tech: ["Python", "Data Sets"],
+    image: "https://via.placeholder.com/300",
+    liveLink: "https://example.com/live",
+    githubLink: "https://github.com/wyattcasss/ValorantML"
+  }
 ];
 
-
 function App() {
+
+
   return (
     <div className="flex justify-center p-4">
       <div className="w-full max-w-4xl">
-      <h1 className="text-3xl font-bold text-center my-8 text-white !important">My Projects</h1>
+        <div className="project-heading">Hey, I'm Wyatt Cassiotis 👋🏻</div>
+        <div className="sub-heading">📍Toronto, Canada 🇨🇦</div>
         {}
-        <div className="grid grid-cols-2 gap-4">
+
+        <div className="project-heading">MY PROJECTS</div>
+        <div className="project-card-container">
           {projects.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
@@ -45,6 +57,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
